35a6573f5dd0d5225912ae177a5be7eea0f1cd2a
Elson Otake <elsonotake@yahoo.com.br>
2022-11-11 19:38:21 -0300

    Add routes to users api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: %i[index show create update destroy]
+    end
+  end
 end
