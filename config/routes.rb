5e1dae743a578c75cb87343a60a15894cd2607d0
Elson Otake <elsonotake@yahoo.com.br>
2022-11-22 11:50:24 -0300

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'api/v1/users#index'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[index show create update destroy]
