66436b541691ba121df58cc6bf1caba163c9376f
Elson Otake <elsonotake@yahoo.com.br>
2022-12-01 19:21:30 -0300

    Update gender validation

--- app/models/leaf.rb
+++ app/models/leaf.rb
@@ -5,6 +5,6 @@ class Leaf < ApplicationRecord
   has_many :leaf1_pairs, class_name: 'Pair', foreign_key: 'leaf1_id'
   has_many :leaf2_pairs, class_name: 'Pair', foreign_key: 'leaf2_id'
   validates :name, presence: true
-  validates :gender, presence: true, inclusion: { in: %w[M F], message: '%<value>s is not a valid gender (M/F)' }
+  validates :gender, presence: true, inclusion: { in: %w[M F P U], message: '%<value>s is not a valid gender (M/F/P/U)' }
   validates :alive, presence: true, inclusion: { in: %w[S N], message: '%<value>s is not a valid input (S/N)' }
 end
