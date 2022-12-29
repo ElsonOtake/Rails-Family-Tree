ba4b4831d0cc9570eeabf0badc41112580b686d8
Elson Otake <elsonotake@yahoo.com.br>
2022-12-29 17:49:58 -0300

    Remove unused code

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,6 +1,4 @@
 class Pair < ApplicationRecord
-  # belongs_to :leaf1_id
-  # belongs_to :leaf2_id
   belongs_to :leaf1, class_name: 'Leaf'
   belongs_to :leaf2, class_name: 'Leaf'
   has_many :branches
